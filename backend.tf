5f80357a6ae7d391b8812294afe37c0a864f2818
Learn Tech With Yannick <126810742+thedevopsguru1@users.noreply.github.com>
2023-07-29 12:19:59 -0500

    Add files via upload

--- backend.tf
+++ backend.tf
@@ -1,15 +1,8 @@
 resource "aws_s3_bucket" "bucket" {
     bucket = "anaeleboo-terraform-state-backend"
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-    object_lock_configuration {
-        object_lock_enabled = "Enabled"
-    }
+ 
+    object_lock_enabled = true
+    
     tags = {
         Name = "S3 Remote Terraform State Store"
     }
@@ -21,5 +14,28 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+# Create Bucket Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Lock configuration
+resource "aws_s3_bucket_object_lock_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
   }
 }
