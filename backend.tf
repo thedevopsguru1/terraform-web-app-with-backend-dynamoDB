77e1be248af0d9e0a4bfc9158b1fd5537251c9aa
Learn Tech With Yannick <126810742+thedevopsguru1@users.noreply.github.com>
2023-07-29 12:05:35 -0500

    Add files via upload

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
     bucket = "anaeleboo-terraform-state-backend"
-    versioning {
-        enabled = true
-    }
     server_side_encryption_configuration {
         rule {
             apply_server_side_encryption_by_default {
@@ -16,4 +13,13 @@ resource "aws_s3_bucket" "bucket" {
     tags = {
         Name = "S3 Remote Terraform State Store"
     }
+}
+
+# Add version of the bucket
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
